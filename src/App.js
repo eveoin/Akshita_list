6fadf4c1de200e78d1dc47cba726b267b6e48ff8
Akshita Savani <akshitasavani0@gmail.com>
2024-01-24 10:54:12 +0530

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [email, setEmail] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(true);
+
+  const loadScript = (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  };
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  const validateEmail = (email) => {
+    const regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    return regex.test(email);
+  };
+
+  const displayRazorpay = async () => {
+    if (!validateEmail(email)) {
+      setIsValidEmail(false);
+      return;
+    }
+
+    setIsValidEmail(true);
+
+    const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js');
+    if (!res) {
+      alert('You are offline...... Failed to load razorpay SDK');
+      return;
+    }
+
+    const options = {
+      key: 'rzp_test_BqwXmtR5v1PWNh',
+      currency: 'INR',
+      amount: 10000,
+      name: 'eveo',
+      description: 'Thanks for joining',
+      handler: function (response) {
+        alert('Payment Successfully');
+        handleClick(); 
+      },
+      prefill: {
+        name: 'eveo',
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-group">
+        <label>Enter your mail:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setIsValidEmail(true);
+          }}
+        />
+        {!isValidEmail && <p style={{ color: 'red' }}>Invalid email address</p>}
+        <button type="button" onClick={() => displayRazorpay()}>
+          Join Now
+        </button>
+      </div>
+      <div>
+        <h2>Waiting List</h2>
+        <button><ul>{count}</ul></button>
+      </div>
     </div>
   );
 }
